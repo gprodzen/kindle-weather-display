4794290cefcf5af51d4e453e2514f204eb2ca783
gprodzen <57318590+gprodzen@users.noreply.github.com>
2022-01-05 11:08:30 -0500

    Updated python to show all day names and forecast report date

--- server/weather-script.py
+++ server/weather-script.py
@@ -7,6 +7,7 @@
 from xml.dom import minidom
 import datetime
 import codecs
+from dateutil import tz
 try:
     # Python 3
     from urllib.request import urlopen
@@ -20,7 +21,8 @@ except ImportError:
 
 latitude = 39.95222000000007
 longitude = -75.16217999999998
-
+from_zone = tz.gettz('UTC')
+to_zone = tz.gettz('America/New_York')
 
 
 #
@@ -30,6 +32,15 @@ longitude = -75.16217999999998
 # Fetch data (change lat and lon to desired location)
 weather_xml = urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + str(latitude) + '&lon=' + str(longitude) + '&format=24+hourly&numDays=4&Unit=e').read()
 dom = minidom.parseString(weather_xml)
+
+# Fetch report datetime
+date = dom.getElementsByTagName('creation-date')
+date_string = date[0].firstChild.nodeValue
+date_string = date_string.replace("T","-")
+date_string = date_string.replace("Z","")
+utc = datetime.datetime.strptime(date_string, '%Y-%m-%d-%H:%M:%S')
+utc = utc.replace(tzinfo=from_zone)
+eastern = utc.astimezone(to_zone)
 
 # Parse temperatures
 xml_temperatures = dom.getElementsByTagName('temperature')
@@ -69,9 +80,13 @@ output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replac
 output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
 output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
 
+#Insert report time
+output = output.replace('LAST_UPDATE',str(eastern)) # Insert forecast report date
+
 # Insert days of week
 one_day = datetime.timedelta(days=1)
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+output = output.replace('DAY_ONE',days_of_week[(day_one + 0*one_day).weekday()]).replace('DAY_TWO',days_of_week[(day_one + 1*one_day).weekday()])
 output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
 
 # Write output
